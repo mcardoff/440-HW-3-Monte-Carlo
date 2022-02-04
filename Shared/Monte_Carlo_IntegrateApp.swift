c89e53e2407db81f5c52b8ea59772d272df721cb
Michael Cardiff <mcardiff@hawk.iit.edu>
2022-02-04 16:47:23 -0600

    Initial Commit

---
+++ Shared/Monte_Carlo_IntegrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monte_Carlo_IntegrateApp.swift
+//  Shared
+//
+//  Created by Michael Cardiff on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Monte_Carlo_IntegrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
